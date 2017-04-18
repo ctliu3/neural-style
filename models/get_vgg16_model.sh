c1e839c5ef98fca1c7a763517259337e8d8735b2
ctliu3 <lcndn3@gmail.com>
2017-04-19 00:06:28 +0800

    models: update download scripts

---
+++ models/get_vgg16_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg16-00b39a1b.pth
+mv vgg16-00b39a1b.pth vgg16-00b39a1b-255.pth
--- models/get_vgg19_model.sh
+++ models/get_vgg19_model.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget https://download.pytorch.org/models/vgg19-dcbb9e9d.pth
+wget https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg19-d01eb7cb.pth
+mv vgg19-d01eb7cb.pth vgg19-d01eb7cb-255.pth
